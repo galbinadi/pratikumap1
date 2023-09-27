7613c106e78aa4d3c5a0a2934def24f0cf6255c1
galbinadi <146154088+galbinadi@users.noreply.github.com>
2023-09-27 14:22:14 +0700

    Add files via upload

---
+++ soal 1 pertemuan 3/soal 1 pertemuan 3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  int a;
+  int b = 2;
+  
+cout << "masukkan bilangan: ";
+cin >> a;
+
+  if (a % b == 0) {
+    cont << " bilangan yang anda input adalah bilangan genap";
+  } else {
+   cont << a << "bilangan yang anda input adalah bilangan ganjil";
+  }
+
+  cout << endl;
+  return 0;
+}
